4484395e69f5da4d256480b0254f573dc796bcca
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-05-21 11:46:52 -0600

    swagger ui working, now needing to fix CORS

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express';
 import {connectToDatabase} from "./database/connection.js";
 import {router} from "./routes/router.js"
 
@@ -7,6 +7,19 @@ const PORT = 8080;
 
 connectToDatabase()
     .then(() => {
+        app.use((request: Request, response: Response, next: NextFunction) => {
+            response.setHeader('Access-Control-Allow-Origin', '*');
+            response.setHeader(
+                'Access-Control-Allow-Headers',
+                'Origin, X-Requested-With, Content-Type, Accept'
+            );
+            response.setHeader(
+                'Access-Control-Allow-Methods',
+                'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+            );
+            next();
+        })
+
         app.use("/", router);
 
         app.listen(PORT, () => {
