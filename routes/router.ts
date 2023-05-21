bba8c227508d68daaee1a0c1a9253d615f92084a
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-05-21 00:01:22 -0600

    Added documentation for routes.

--- routes/router.ts
+++ routes/router.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import {listAllTimers, createNewTimer} from "../controllers/timers.js";
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerDocument from "../utils/swagger-output.json" assert { type: "json"}
+
 
 export const router = express.Router();
 
 router.use(express.json())
+router.use('/api-docs', swaggerUi.serve);
+
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 router.get("/timers", listAllTimers);
 
