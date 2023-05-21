b4cd429366a37c916d02687b7dfd7fdd86f8cd9e
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-05-21 01:34:08 -0600

    Changed json parser.

--- routes/router.ts
+++ routes/router.ts
@@ -2,11 +2,11 @@ import express from "express";
 import {listAllTimers, createNewTimer} from "../controllers/timers.js";
 import swaggerUi from 'swagger-ui-express'
 import * as swaggerDocument from "../utils/swagger-output.json" assert { type: "json"}
-
+import bodyParser from "body-parser";
 
 export const router = express.Router();
 
-router.use(express.json())
+router.use(bodyParser.json())
 router.use('/api-docs', swaggerUi.serve);
 
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
